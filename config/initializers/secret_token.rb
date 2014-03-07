45cd21583a42dfac8e4f6f91203207afd99a24ca
Robert Martin <robertmartinbc@shaw.ca>
2014-03-07 10:07:34 -0800

    first commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignsOfHope::Application.config.secret_token = '6e7583a190f8526d84900f729aa3e2cdf28d76a235c7c0fb6f75e6fa6a7e56341aa4c161d49299d6b124ac57f43197aeb1614456b541f927e661836a5f58a601'
